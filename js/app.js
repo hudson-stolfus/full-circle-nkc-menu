ff8807d846134b495aa7e4e5beb2a71840169d15
Hudson Stolfus <hudson.stolfus@gmail.com>
2025-04-30 10:09:50 -0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,74 @@
+fetch('/data/functions.json').then(response => response.json()).then((functions) => {
+    const upcoming = document.getElementById('functions-dynamic');
+    for (let date of Object.keys(functions)) {
+        if (new Date(date) > new Date().setHours(23, 59, 59)) {
+            let functionEl = document.createElement('div');
+            functionEl.classList.add('card');
+
+            let heading = document.createElement('h3');
+            heading.classList.add('card-heading');
+            let icon = document.createElement('i');
+            icon.setAttribute('data-lucide', functions[date].icon);
+            icon.classList.add('card-icon');
+            heading.appendChild(icon);
+            let name = document.createElement('div');
+            name.classList.add('card-name');
+            name.appendChild(document.createTextNode(functions[date].name))
+            heading.appendChild(name);
+            let price = document.createElement('div');
+            price.classList.add('card-price');
+            price.textContent = `$${functions[date].cost ?? '?'}`;
+            heading.appendChild(price);
+
+            let content = document.createElement('div');
+            content.classList.add('card-content');
+            content.appendChild(document.createTextNode(`${new Date(date).toLocaleString('en-US', {
+                month: 'short',
+                day: 'numeric',
+                year: 'numeric',
+                hour: 'numeric',
+                minute: '2-digit'
+            })}`));
+            content.appendChild(document.createElement('br'));
+            content.appendChild(document.createTextNode(functions[date].location));
+            functionEl.appendChild(heading);
+            functionEl.appendChild(content);
+            upcoming.appendChild(functionEl);
+        }
+    }
+    lucide.createIcons();
+});
+
+fetch('/data/menu.json').then(response => response.json()).then((menu) => {
+    for (let category of Object.keys(menu)) {
+        for (let item of menu[category].items) {
+            let itemEl = document.createElement('div');
+            itemEl.classList.add('menu-item');
+            if (!item.stocked) itemEl.classList.add('sold-out');
+
+            let heading = document.createElement('h3');
+            heading.classList.add('card-heading');
+            let icon = document.createElement('i');
+            icon.setAttribute('data-lucide', item.icon);
+            icon.classList.add('card-icon');
+            heading.appendChild(icon);
+            let name = document.createElement('div');
+            name.classList.add('card-name');
+            name.appendChild(document.createTextNode(item.name))
+            heading.appendChild(name);
+            let price = document.createElement('div');
+            price.classList.add('card-price');
+            price.textContent = `$${item.price.toFixed(2)}`;
+            heading.appendChild(price);
+
+            let content = document.createElement('div');
+            content.classList.add('card-content');
+            content.appendChild(document.createTextNode(item.options.join(', ')));
+            itemEl.appendChild(heading);
+            itemEl.appendChild(content);
+            document.getElementById(category).appendChild(itemEl);
+
+            lucide.createIcons();
+        }
+    }
+});
